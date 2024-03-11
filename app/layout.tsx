8ed4b4853918c3adae3ad8a335e3c1149e42b64c
yuanqiang.luo <yuanqiang.luo>
2024-03-11 19:40:04 +0800

    调整布局

--- app/layout.tsx
+++ app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({ children }: Readonly<RootLayoutProps>) {
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
               <div className="flex-1">{children}</div>
-        <footer className="max-w-screen-xl mx-auto px-4    md:px-8">
+        <footer className="max-w-screen-xl mx-auto px-4  md:px-8">
                 <div className="mt-3 py-3 border-t md:text-center">
                     <p>© 2024 备案号. All rights reserved.</p>
                 </div>
